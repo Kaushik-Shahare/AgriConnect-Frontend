c6d88237c3d4ed091caa1fa0f32db6726d7f653a
Kaushik-Shahare <kaushikshahare0@gmail.com>
2024-12-04 13:08:52 +0530

    Updated BACKEND_URL

--- src/context/ConstantsContext.jsx
+++ src/context/ConstantsContext.jsx
@@ -8,7 +8,7 @@ const ConstantsContext = createContext();
 const ConstantsProvider = ({ children }) => {
   const constants = {
     // BACKEND_URL: "http://localhost:8000",
-    BACKEND_URL: "https://agri-connect-delta.vercel.app",
+    BACKEND_URL: "https://api-agriconnect.kaushikshahare.com",
 
     // Add more constants as needed
   };
